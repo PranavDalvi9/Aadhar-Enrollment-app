38adaf205264452cdfe416021347e1e82074bd99
PranavDalvi9 <96105012+PranavDalvi9@users.noreply.github.com>
2022-06-11 19:07:37 +0530

    controller updated

--- server/src/controllers/AdharController.js
+++ server/src/controllers/AdharController.js
@@ -24,4 +24,21 @@ router.get("", async (req, res) => {
   }
 });
 
+router.post("/login", async (req, res) => {
+  try {
+    const data = await AdharDetail.findOne({ email: req.body.email });
+    const pass = data.password === req.body.password;
+
+    if (pass) {
+      return res.send(data);
+    }
+
+    return res
+      .status(400)
+      .send({ message: "Please try another email or password" });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
 module.exports = router;
